2595621074244a8592dbcaec185d4e53c3c695e0
lyt57 <77660691+lyt57@users.noreply.github.com>
2023-07-19 11:06:03 +0800

    Update Motif_analysis.sh

--- Motif_analysis.sh
+++ Motif_analysis.sh
@@ -1,1 +1,14 @@
 # De novo motif identification
+function getmotif(){
+    i=$1
+    jaspar=./JASPAR2022_CORE_plants_non-redundant_pfms_meme.txt
+    meme-chip -meme-minw 5 -meme-maxw 12 -meme-nmotifs 3 -meme-p 20 -dreme-m 0 -spamo-skip -fimo-skip -norc -oc meme_chip/${file/.fa/} ${file} -db ${jaspar}
+    genome=./B73v4.fa
+}
+
+# motif enrichment
+cat filelist.txt |while read name ;do 
+	mkdir $name/
+	bedtools getfasta -fi B73v4.fa -fo $name".fa" -s -name -bed $id
+	ame --verbose 1 --o $name/ --scoring avg --method fisher --hit-lo-fraction 0.25 --evalue-report-threshold 10.0 --control --shuffle-- --kmer 2 $name.fa ./jaspar_plantfdb_DAP_uniq.meme
+done
